99267255008ff4d66f5c547f2df92a6d8a8d6151
Jun Sok Huhh <anarinsk@gmail.com>
2019-05-23 13:21:00 +0900

    add gradientdsc.py

---
+++ gradientdsc.py
@@ -0,0 +1,48 @@
+def update_w_and_b(spendings, sales, w, b, alpha):    
+
+    dl_dw = 0.0
+    dl_db = 0.0
+    N = len(spendings)
+
+    for i in range(N):
+        dl_dw += -2*spendings[i]*(sales[i] - (w*spendings[i] + b))
+        dl_db += -2*(sales[i] - (w*spendings[i] + b))
+
+    # update w and b
+
+    w = w - (1/float(N))*dl_dw*alpha
+    b = b - (1/float(N))*dl_db*alpha
+
+    return w, b
+
+def train(spendings, sales, w, b, alpha, epochs):
+    for e in range(epochs):
+        w, b = update_w_and_b(spendings, sales, w, b, alpha)
+    # log the progress
+        if e % 400 == 0:
+            print("epoch:", e, "loss: ", avg_loss(spendings, sales, w, b))
+    return w, b
+
+def avg_loss(spendings, sales, w, b):
+    N = len(spendings)
+    total_error = 0.0
+    for i in range(N):
+        total_error += (sales[i] - (w*spendings[i] + b))**2
+    return total_error / float(N)
+
+
+
+with open('data/Advertising.csv', 'r') as f:
+    reader = csv.reader(f)
+    my_list = list(reader)
+
+import pandas as pd 
+df = pd.read_csv('data/Advertising.csv', delimiter=',')
+
+df
+
+train(df.radio, df.sales, 0.0, 0.0, 0.001, 15000)
+
+%reset
+
+
